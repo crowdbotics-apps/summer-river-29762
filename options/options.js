f687b952e9715860c38024fc50b4bed8a86e38cc
Crowdbotics <team@crowdbotics.com>
2021-08-12 15:42:51 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "summer_river_29762Identifier",
+  url: "https://summer_river_29762Identifier.botics.co",
+  api: "https://summer_river_29762Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
